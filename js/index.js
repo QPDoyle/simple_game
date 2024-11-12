6c8097b9972d96d10cfdedf6b885079e4e363204
QPDoyle <qpdoyle13@gmail.com>
2024-11-12 11:11:49 -0500

    can flip tile1 back and forth

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function Tile(color) {
     this.color = color;
 }
-const tiles = [];
+
 const totalFlips = document.getElementById('totalFlips');
 const tile1 = document.getElementById('tile1');
 const tile2 = document.getElementById('tile2');
@@ -11,20 +11,29 @@ const tile5 = document.getElementById('tile5');
 const tile6 = document.getElementById('tile6');
 const tile7 = document.getElementById('tile7');
 const tile8 = document.getElementById('tile8');
+const tiles = [];
+
+const colors = ['blue', 'blue', 'green', 'green', 'red', 'red', 'yellow', 'yellow'];
+for (i in colors) {
+    const newTile = new Tile(colors[i]);
+    tiles.push(newTile);
+}   
+
 
 //TO FIX
 //  add a listener for each box
-mole.addEventListener('click', whack);
+
 
 // on click, change box class color to assigned color
     // on click get tile id 
 
+tile1.addEventListener('click', e =>{
+    if(tile1.className === 'unFlipped'){
+        console.log("click");
+        tile1.className = colors[0];
+    }
+    else {
+        tile1.className = 'unFlipped';
+    }
+})
 
-const colors = ['blue', 'blue', 'green', 'green', 'red', 'red', 'yellow', 'yellow'];
-for (i in colors) {
-    tiles.append[Tile(colors[i])];
-}   
-
-const flip = () => {
-    //choose a random tile to flip first
-}
